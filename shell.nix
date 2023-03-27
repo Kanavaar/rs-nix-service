2609d6b9b4e396848d1a6ce60564d414c7949764
kanavaar <uju8765@gmail.com>
2023-03-27 13:48:02 +0200

    fixing dependencies

--- shell.nix
+++ shell.nix
@@ -1,14 +1,11 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/a1c45f32e6450f052525fec74b9db0154844c638.tar.gz) {} }:
 
 pkgs.mkShell {
+  nativeBuildInputs = with pkgs; [ pkg-config ]
   buildInputs = with pkgs; [
     cargo
     mold
     openssl
-    pkg-config
+    libressl
   ];
-
-  shellHook = ''
-    pkg-config
-  ''
 }
