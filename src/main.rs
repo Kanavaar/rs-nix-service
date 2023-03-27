825a8d6ee06d5b609f21a5321efecabe1c7aa628
kanavaar <uju8765@gmail.com>
2023-03-27 14:12:30 +0200

    getting image url

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,22 @@
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct CatImage {
+    url: String,
+}
+
 #[tokio::main]
 async fn main() {
     let respons = reqwest::get("https://api.thecatapi.com/v1/images/search")
         .await
         .unwrap();
-    println!("Status: {}", respons.status());
-    let body = respons.text().await.unwrap();
-    println!("Body: {}", body);
+    if !respons.status().is_success() {
+        panic!("Request failed with HTTP {}", respons.status());
+    }
+
+    let images: Vec<CatImage> = respons.json().await.unwrap();
+    let image = images
+        .first()
+        .expect("Cat api should return at least one image");
+    println!("Image is at: {}", image.url);
 }
